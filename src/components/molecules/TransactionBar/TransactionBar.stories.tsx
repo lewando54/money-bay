b93bb6a4ac1b6da4576c3d76a0024ea7a1006a59
Jakub Lewandowski <lewando54@gmail.com>
2023-09-29 17:15:35 +0200

    feat: Add TransactionBar
    Added TransactionBar component with stories, tests, styling.

---
+++ src/components/molecules/TransactionBar/TransactionBar.stories.tsx
@@ -0,0 +1,46 @@
+﻿/* eslint-disable @typescript-eslint/consistent-type-assertions */
+import { StoryObj, Meta } from '@storybook/react-native'
+
+import TransactionBar from './TransactionBar'
+
+const meta = {
+	component: TransactionBar,
+	argTypes: {
+		title: { control: 'text' },
+		subTitle: { control: 'text' },
+		amount: { control: 'number' },
+		percent: { control: 'number' },
+		isExpense: { control: 'boolean' }
+	}
+} satisfies Meta<typeof TransactionBar>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+	args: {
+		title: 'Adalyn Roth',
+		subTitle: 'Money transfer',
+		amount: 140.00,
+		percent: 20,
+		isExpense: true
+	}
+}
+
+export const WithoutPercentSign: Story = {
+	args: {
+		title: 'Adalyn Roth',
+		subTitle: 'Money transfer',
+		amount: 140.00,
+		isExpense: true
+	}
+}
+
+export const IsAnIncome: Story = {
+	args: {
+		title: 'Adalyn Roth',
+		subTitle: 'Money transfer',
+		amount: 140.00,
+		isExpense: false
+	}
+}
---
+++ src/components/molecules/TransactionBar/TransactionBar.tsx
@@ -0,0 +1,43 @@
+﻿import { View, Text } from 'react-native'
+import CircleWithIcon from '../../atoms/CircleWithIcon/CircleWithIcon'
+import UniversalContainer from '../../atoms/UniversalContainer/UniversalContainer'
+import transactionBarStyle from './TransactionBar.style'
+import { MaterialIcons } from '@expo/vector-icons'
+import globalThemeStyle, { BODY_TEXT_COLOR, GOOD_GREEN, MAIN_DARK } from '../../../styling/GlobalTheme.style'
+
+interface ITransactionBarProps {
+    icon?: React.ReactNode
+    title?: string
+    subTitle?: string
+    amount?: number
+    percent?: number
+    isExpense?: boolean
+    testID?: string
+}
+
+export default function TransactionBar({
+    icon=<MaterialIcons name="compare-arrows" size={24} color="black" />,
+    title='Adalyn Roth',
+    subTitle='Money transfer',
+    amount=140.00,
+    percent,
+    isExpense=true,
+    testID
+}: ITransactionBarProps){
+    return (
+        <UniversalContainer testID={testID} style={{flexDirection: 'row', alignItems: 'center', gap: 13}}>
+            <CircleWithIcon icon={icon} size={40}/>
+            <View>
+                <Text style={globalThemeStyle.text_Title}>{title}</Text>
+                <Text style={globalThemeStyle.text_SubTitle}>{subTitle}</Text>
+            </View>
+            <View style={[{flexDirection: 'row', marginLeft: 'auto', gap: 5}]}>
+                <Text style={[globalThemeStyle.text_Title, !isExpense && {color: GOOD_GREEN}]}>{isExpense ? '-' : '+'}</Text>
+                <View>
+                    <Text style={[globalThemeStyle.text_Title, !isExpense && {color: GOOD_GREEN}]}>{amount.toFixed(2)}</Text>
+                    <Text style={[globalThemeStyle.text_SubTitle, {marginLeft: 'auto'}, !percent && {display: 'none'}]}>{percent+'%'}</Text>
+                </View>
+            </View>
+        </UniversalContainer>
+    )
+}
