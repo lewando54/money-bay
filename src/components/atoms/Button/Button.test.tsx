c4a387cb2d902c1f9e0002b68a869dca53ece4f7
Jakub Lewandowski <lewando54@gmail.com>
2023-11-13 20:07:41 +0100

    refactor: remove xd from names

--- src/components/atoms/Button/Button.test.tsx
+++ src/components/atoms/Button/Button.test.tsx
@@ -7,17 +7,17 @@ import Button from './Button'
 
 describe('Button', () => {
     it('should render correctly', () => {
-        const tree = renderer.create(<Button testID='testxd' color="primary">Click me</Button>).toJSON()
+        const tree = renderer.create(<Button testID='test' color="primary">Click me</Button>).toJSON()
         expect(tree).toMatchSnapshot()
     })
 
     it('should render with primary color by default', () => {
-        const tree = renderer.create(<Button testID='testxd'>Click me</Button>).toJSON()
+        const tree = renderer.create(<Button testID='test'>Click me</Button>).toJSON()
         expect(tree).toMatchSnapshot()
     })
 
     it('should render innerHTML text', () => {
-        const { getByText } = render(<Button testID='testxd' color="secondary">Click me</Button>)
+        const { getByText } = render(<Button testID='test' color="secondary">Click me</Button>)
         expect(getByText('Click me')).toBeTruthy()
     })
 
@@ -40,19 +40,19 @@ describe('Button', () => {
     })
 
     it('should render small variant', () => {
-        const tree = renderer.create(<Button testID='testxd' color="primary" size={'small'}>Click me</Button>).toJSON()
+        const tree = renderer.create(<Button testID='test' color="primary" size={'small'}>Click me</Button>).toJSON()
         expect(tree).toMatchSnapshot()
     })
 
     it('should render normal variant', () => {
-        const tree = renderer.create(<Button testID='testxd' color="primary" size={'normal'}>Click me</Button>).toJSON()
+        const tree = renderer.create(<Button testID='test' color="primary" size={'normal'}>Click me</Button>).toJSON()
         expect(tree).toMatchSnapshot()
     })
 
     it('should call onClick function when clicked', () => {
         const onClick = jest.fn()
-        const { getByTestId } = render(<Button testID='testxd' onClick={onClick}>Click me</Button>)
-        fireEvent.press(getByTestId('testxd'))
+        const { getByTestId } = render(<Button testID='test' onClick={onClick}>Click me</Button>)
+        fireEvent.press(getByTestId('test'))
         expect(onClick).toHaveBeenCalled()
     })
 })
