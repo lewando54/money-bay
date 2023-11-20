4f26eca66ff48b841228fed4ac6347371e63b401
Jakub Lewandowski <lewando54@gmail.com>
2023-11-20 14:56:59 +0100

    refactor: add OnboardingSlide stories

--- src/components/molecules/OnboardingSlide/OnboardingSlide.stories.tsx
+++ src/components/molecules/OnboardingSlide/OnboardingSlide.stories.tsx
@@ -6,7 +6,13 @@ import OnboardingSlide from './OnboardingSlide'
 const meta = {
     component: OnboardingSlide,
     argTypes: {
-        // To write
+        id: {control: 'number'},
+        index: {control: 'number'},
+        imageSource: { control: 'select', options: [require('./assets/onboarding-2.png'), require('@assets/img/onboarding-3.png'), require('@assets/img/onboarding-4.png')]},
+        style: {control: 'object'},
+        heading: {control: 'text'},
+        opacity: {control: 'number'},
+        body: {control: 'text'},
     }
 } satisfies Meta<typeof OnboardingSlide>
 
@@ -15,6 +21,10 @@ type Story = StoryObj<typeof OnboardingSlide>
 
 export const Default: Story = {
     args: {
-        // To write
+        id: 0,
+        index: 0,
+        imageSource: require('./assets/onboarding-2.png'),
+        heading: 'Welcome to MoneyBay app!',
+        body: 'Labore sunt culpa excepteur culpa ipsum. Labore occaecat ex nisi mollit.',
     }
 }
