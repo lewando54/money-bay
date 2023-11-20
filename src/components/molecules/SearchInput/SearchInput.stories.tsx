37b3cc7eed541f21ca0481414c9bdf7e8dba42b4
Jakub Lewandowski <lewando54@gmail.com>
2023-11-20 15:19:34 +0100

    refactor: add SearchInput stories

--- src/components/molecules/SearchInput/SearchInput.stories.tsx
+++ src/components/molecules/SearchInput/SearchInput.stories.tsx
@@ -1,20 +1,65 @@
 ﻿/* eslint-disable @typescript-eslint/consistent-type-assertions */
+import React, { useState } from 'react'
+import { StyleProp, TextStyle, ViewStyle } from 'react-native'
 import { Meta,StoryObj } from '@storybook/react-native'
 
 import SearchInput from './SearchInput'
 
+interface ISearchInputMockupProps {
+    value?: string
+    placeholder?: string
+    containerStyle?: StyleProp<ViewStyle>
+    textStyle?: StyleProp<TextStyle>
+    onIconPress?: () => void
+}
+
+function StatefulSearchInput({
+    value,
+    placeholder='Search',
+    containerStyle,
+    textStyle,
+    onIconPress,
+}: ISearchInputMockupProps){
+    const [svalue, setsvalue] = useState(value)
+
+    return (
+        <SearchInput 
+            value={svalue}
+            placeholder={placeholder}
+            containerStyle={containerStyle}
+            textStyle={textStyle}
+            onIconPress={onIconPress}
+            onChange={(newValue) => setsvalue(() => newValue)}
+        />
+    )
+}
+
 const meta = {
-    component: SearchInput,
+    component: StatefulSearchInput,
     argTypes: {
-        // To write
+        value: {control: 'text'},
+        placeholder: {control: 'text'},
+        containerStyle: {control: 'object'},
+        textStyle: {control: 'object'},
+        onIconPress: {action: 'iconPressed'}
     }
-} satisfies Meta<typeof SearchInput>
+} satisfies Meta<typeof StatefulSearchInput>
 
 export default meta
-type Story = StoryObj<typeof SearchInput>
+type Story = StoryObj<typeof StatefulSearchInput>
 
 export const Default: Story = {
     args: {
-        // To write
+        value: 'Example input',
+        placeholder: 'Search',
+    }
+}
+
+export const CustomStyles: Story = {
+    args: {
+        value: 'Example input',
+        placeholder: 'Search',
+        textStyle: {color: 'red'},
+        containerStyle: {backgroundColor: 'blue'}
     }
 }
