e7fa3eaf54d4d00caf89392d51d394e51070442c
Jakub Lewandowski <lewando54@gmail.com>
2023-11-22 15:06:47 +0100

    refactor: email valid. by zod

--- src/components/molecules/EmailInput/EmailInput.tsx
+++ src/components/molecules/EmailInput/EmailInput.tsx
@@ -1,6 +1,7 @@
 ﻿import React, { useState } from 'react'
 import { StyleProp, TextStyle,ViewStyle } from 'react-native'
 import { Feather } from '@expo/vector-icons'
+import { z } from 'zod'
 
 import { Input } from '@atoms'
 
@@ -24,17 +25,18 @@ export default function EmailInput({
     testID
 }: IEmailInputProps){
     const [ isValid, setIsValid ] = useState(false)
+    const emailSchema = z.string().email()
 
     const handleOnChange = (newValue: string) => {
         if(onChange){
             onChange(newValue)
         }
 
-        // eslint-disable-next-line no-control-regex
-        if(newValue.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)){
+        try{
+            emailSchema.parse(newValue)
             setIsValid(() => true)
         }
-        else{
+        catch(err){
             setIsValid(() => false)
         }
     }
